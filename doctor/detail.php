bf2a52ca7e37e6c1bd4d0a2bf8ea694d600f7219
najlaalariki <najlaalariki39@gmail.com>
2024-04-30 00:14:00 +0300

    Merge the project

--- doctor/detail.php
+++ doctor/detail.php
@@ -5,112 +5,81 @@
   include_once('../includes/patient.php');
   include_once('../includes/doctor.php');
   include_once('../includes/nurse.php');
-  include_once('../includes/setting.php');
-  checkPatientSession();
+
+  checkDoctorSession();
+
+  $pageTitle = lang("Appointment Details");
+  $row = new Appointment(null);
+  include('../template/header.php');
 
 
-  
-  $pageTitle = lang("Add Appointment");
-  include('../template/header.php'); 
-  $errors = array();
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') 
+  {
 
-  $setting = GetSetting();
-  if( count($setting) > 0 ){
-    $setting = $setting[0];
-  }
-  else{
-    redirectToReferer(lang("You shloud to do setting for website fisrt"));
+    if(isset($_GET['id']))
+    {
+      $id = $_GET['id'];
+      $result = getAppointmentById($id);
+
+      if( count( $result ) > 0)
+        $row = $result[0];
+
+      if($row == null)
+      {
+          $_SESSION["message"] = lang('There is No data for this id');
+          $_SESSION["fail"] = lang('There is No data for this id');
+      }
+
+    }
+    else
+    {
+      $_SESSION["message"] = lang('No data for display');
+      $_SESSION["fail"] = lang('No data for display');
+    }
+
   }
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST') 
   {
-    if(isset($_POST['addAppointment']))
+    if(isset($_POST['deleteAppointment']))
     {
-
-
-      $detail = $_POST['detail'];
-
-      $patient_id = $_POST['patient_id'];
-
-      $doctor_id = $_POST['doctor_id'] ?? null;
-
-      $nurse_id = $_POST['nurse_id'] ?? null;
-
-      $appointment_date = $_POST['appointment_date'];
-
-      $appointment_time = $_POST['appointment_time'];
-
-      $price = $_POST['price'];
-
-      $state = $_POST['state'];
-
-     // $created_date = $_POST['created_date'];
-
-      if( empty($detail)){
-        $errors[] = "<li>" . lang("Detail is requierd") . "</li>";
-        $_SESSION["fail"] .= "<li>" . lang("Detail is requierd") . "</li>";
-        }
-      if( empty($patient_id)){
-        $errors[] = "<li>" . lang("Patient is requierd") . "</li>";
-        $_SESSION["fail"] .= "<li>" . lang("Patient is requierd") . "</li>";
-        }
-      if( empty($appointment_date)){
-        $errors[] = "<li>" . lang("Appointment Date is requierd") . "</li>";
-        $_SESSION["fail"] .= "<li>" . lang("Appointment Date is requierd") . "</li>";
-        }
-      if( empty($appointment_time)){
-        $errors[] = "<li>" . lang("Appointment Time is requierd") . "</li>";
-        $_SESSION["fail"] .= "<li>" . lang("Appointment Time is requierd") . "</li>";
-        }
-      if( empty($price)){
-        $errors[] = "<li>" . lang("Price is requierd") . "</li>";
-        $_SESSION["fail"] .= "<li>" . lang("Price is requierd") . "</li>";
-        }
-      if( empty($state)){
-        $errors[] = "<li>" . lang("State is requierd") . "</li>";
-        $_SESSION["fail"] .= "<li>" . lang("State is requierd") . "</li>";
-        }
-      /*if( empty($created_date)){
-        $errors[] = "<li>" . lang("Created Date is requierd") . "</li>";
-        $_SESSION["fail"] .= "<li>" . lang("Created Date is requierd") . "</li>";
-        }*/
-  
-      if(count($errors) == 0)
+      if(isset($_GET['id']))
       {
-        $add = addAppointment(
-                                    $detail,
-                                    $patient_id,
-                                    $doctor_id,
-                                    $nurse_id,
-                                    $appointment_date,
-                                    $appointment_time,
-                                    $price,
-                                    $state,
-                                    date('Y-m-d H:i:s'),
-                                    );
-        if($add ==  true)
+        $id = $_POST['id'];
+        $delete = deleteAppointment($id);
+        if($delete ==  true)
         {
-          $_SESSION["message"] = lang("Appointment Added successfuly!");
-          $_SESSION["success"] = lang("Appointment Added successfuly!");
-          header('Location:'. $PATH_PATIENT .'index.php');
+  
+          $_SESSION["message"] = lang("Appointment Deleted successfuly!");          
+          $_SESSION["success"] = lang("Appointment Deleted successfuly!");          
+          header('Location:'. $PATH_DOCTOR_APPOINTMENT .'index.php');
           exit();
         }
         else
         {
-          $_SESSION["message"] = lang("Error when Adding Data");
-          $_SESSION["fail"] = lang("Error when Adding Data");
-          $errors[] = lang("Error when Adding Data");
+          $_SESSION["message"] = lang("Error when Delete Data");
+          $_SESSION["fail"] = lang("Error when Delete Data");
+
+          $errors[] = lang("Error when Delete Data");
         }
-        
       }
-  
+      else
+      {
+        $_SESSION["message"] = lang('No data for Delete');
+        $_SESSION["fail"] = lang('No data for Delete');
+      }
     }
+    else
+    {
+      $_SESSION["message"] = lang('No data for Delete');
+      $_SESSION["fail"] = lang('No data for Delete');
+    }
+
   }
+
 ?>
 
 <?php include('../template/startNavbar.php'); ?>
-
-
 
 <!-- Content -->
 <main>
@@ -121,7 +90,7 @@
                     <div class="col-auto mb-3">
                         <h1 class="page-header-title">
                             <div class="page-header-icon"><i class="fa fa-school"></i></div>
-                           <?php echo lang("Add Appointment"); ?>
+                            <?php echo lang("Appointment Details"); ?>
                         </h1>
                     </div>
                     <div class="col-12 col-xl-auto mb-3">
@@ -145,78 +114,81 @@
                         <form action="" method="POST" enctype="multipart/form-data">
                             <!-- Form Row-->
                             <div class="row gx-3 mb-3">
+                                <input type="hidden" name="id" id="id" value="<?php echo $row['id'];?>" readonly />
                                 <!-- Form Group (detail)-->
                                 <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="detail"><?php echo lang("Detail"); ?></label>
                                     <input class="form-control" id="detail" name="detail" type="text" placeholder="<?php echo lang("Detail"); ?>"
-                                        value="" required  />
+                                        value="<?php echo $row['detail'];?>" readonly />
                                 </div>
                                 <!-- Form Group (patient_id)-->
-                                <div class="col-md-4 mb-3 d-none">
+                                <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="patient_id"><?php echo lang("Patient"); ?></label>
-                                    <select class="form-select" name="patient_id" id="patient_id" required>
-                                        <option selected disabled value=""><?php echo lang("Select a Patient"); ?>:</option>
+                                    <select disabled class="form-select" name="patient_id" id="patient_id" required>
+                                        <option disabled value=""><?php echo lang("Select a Patient"); ?>:</option>
                                         <?php foreach(getAllPatients() as $Patient) { ?>
-                                        <option <?php if($_SESSION['userID'] == $Patient['id']) echo "selected" ?> value="<?php echo $Patient['id']; ?>"> <?php echo $Patient['first_name']; ?>
+                                        <option <?php if($row['patient_id'] == $Patient['id']) echo "selected" ?> value="<?php echo $Patient['id']; ?>"> <?php echo $Patient['first_name']; ?>
                                         </option>
                                         <?php }?>
                                     </select>
                                 </div>
-
                                 <!-- Form Group (doctor_id)-->
                                 <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="doctor_id"><?php echo lang("Doctor"); ?></label>
-                                    <select class="form-select" name="doctor_id" id="doctor_id" >
-                                        <option selected value=""><?php echo lang("Select a Doctor"); ?>:</option>
+                                    <select disabled class="form-select" name="doctor_id" id="doctor_id" >
+                                        <option disabled value=""><?php echo lang("Select a Doctor"); ?>:</option>
                                         <?php foreach(getAllDoctors() as $Doctor) { ?>
-                                        <option value="<?php echo $Doctor['id']; ?>"> <?php echo $Doctor['first_name']; ?>
+                                        <option <?php if($row['doctor_id'] == $Doctor['id']) echo "selected" ?> value="<?php echo $Doctor['id']; ?>"> <?php echo $Doctor['first_name']; ?>
                                         </option>
                                         <?php }?>
                                     </select>
                                 </div>
-
                                 <!-- Form Group (nurse_id)-->
                                 <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="nurse_id"><?php echo lang("Nurse"); ?></label>
-                                    <select class="form-select" name="nurse_id" id="nurse_id" >
-                                        <option selected value=""><?php echo lang("Select a Nurse"); ?>:</option>
+                                    <select disabled class="form-select" name="nurse_id" id="nurse_id" >
+                                        <option disabled value=""><?php echo lang("Select a Nurse"); ?>:</option>
                                         <?php foreach(getAllNurses() as $Nurse) { ?>
-                                        <option value="<?php echo $Nurse['id']; ?>"> <?php echo $Nurse['first_name']; ?>
+                                        <option <?php if($row['nurse_id'] == $Nurse['id']) echo "selected" ?> value="<?php echo $Nurse['id']; ?>"> <?php echo $Nurse['first_name']; ?>
                                         </option>
                                         <?php }?>
                                     </select>
                                 </div>
-
                                 <!-- Form Group (appointment_date)-->
                                 <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="appointment_date"><?php echo lang("Appointment Date"); ?></label>
                                     <input class="form-control" id="appointment_date" name="appointment_date" type="date" placeholder="<?php echo lang("Appointment Date"); ?>"
-                                        value="" required  />
+                                        value="<?php echo $row['appointment_date'];?>" readonly />
                                 </div>
                                 <!-- Form Group (appointment_time)-->
                                 <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="appointment_time"><?php echo lang("Appointment Time"); ?></label>
                                     <input class="form-control" id="appointment_time" name="appointment_time" type="text" placeholder="<?php echo lang("Appointment Time"); ?>"
-                                        value="" required  />
+                                        value="<?php echo $row['appointment_time'];?>" readonly />
                                 </div>
                                 <!-- Form Group (price)-->
                                 <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="price"><?php echo lang("Price"); ?></label>
                                     <input class="form-control" id="price" name="price" type="text" placeholder="<?php echo lang("Price"); ?>"
-                                        value="<?php echo $setting['appointment_price']; ?>" required  readonly />
+                                        value="<?php echo $row['price'];?>" readonly />
                                 </div>
                                 <!-- Form Group (state)-->
-                                <div class="col-md-4 mb-3 d-none">
+                                <div class="col-md-4 mb-3">
                                     <label class="small mb-1" for="state"><?php echo lang("State"); ?></label>
                                     <input class="form-control" id="state" name="state" type="text" placeholder="<?php echo lang("State"); ?>"
-                                        value="request" required  />
+                                        value="<?php echo $row['state'];?>" readonly />
                                 </div>
                                 <!-- Form Group (created_date)-->
-                                
+                                <div class="col-md-4 mb-3">
+                                    <label class="small mb-1" for="created_date"><?php echo lang("Created Date"); ?></label>
+                                    <input class="form-control" id="created_date" name="created_date" type="date" placeholder="<?php echo lang("Created Date"); ?>"
+                                        value="<?php echo $row['created_date'];?>" readonly />
+                                </div>
+ 
                             </div>
                             <!-- Submit button-->
-                            <button name="addAppointment" class="btn btn-success" type="submit"><?php echo lang("Save"); ?></button>
-                            <a href="index.php" class="btn btn-danger" type="button"><?php echo lang("Back To List"); ?></a>
+                            <a href="edit.php?id=<?php echo $row['id'];?>" class="btn btn-success" type="button"><?php echo lang("Edit"); ?></a>
+                            <a href="index.php" class="btn btn-primary" type="button"><?php echo lang("Back To List"); ?></a>
                         </form>
                     </div>
                 </div>
@@ -224,9 +196,6 @@
         </div>
     </div>
 </main>
-
+<!-- Footer -->
 
 <?php include('../template/footer.php'); ?>
-
-
-
