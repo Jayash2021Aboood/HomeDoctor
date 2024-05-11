a6fc04c8033ac3b3877273219b8b93d1e32d19c9
Abdulmalek Jayash <AbdulmalekJayash@gmail.com>
2024-05-11 08:32:44 +0300

    update database

--- homedoctor.sql
+++ homedoctor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 20, 2024 at 03:40 PM
+-- Generation Time: May 11, 2024 at 06:08 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -64,16 +64,19 @@ CREATE TABLE `appointment` (
 --
 
 INSERT INTO `appointment` (`id`, `detail`, `patient_id`, `doctor_id`, `nurse_id`, `appointment_date`, `appointment_time`, `price`, `state`, `created_date`) VALUES
-(1, 's', 8, 4, 5, '2024-04-26', '4', 200, 'request', '2024-04-19 00:00:00'),
-(2, 'سحب دم وقياس الضغط', 8, 4, 5, '2024-04-26', 'الساعة 4 العصر', 200, 'request', '2024-04-19 00:00:00'),
-(3, 'e', 8, 4, 4, '2024-04-26', 'الساعة 4 العصر', 200, 'request', '2024-04-19 00:00:00'),
+(1, 's', 8, 4, 5, '2024-04-26', '4', 200, 'accept', '2024-04-19 00:00:00'),
+(2, 'سحب دم وقياس الضغط', 8, 4, 5, '2024-04-26', 'الساعة 4 العصر', 200, 'accept', '2024-04-19 00:00:00'),
+(3, 'e', 8, 4, 4, '2024-04-26', 'الساعة 4 العصر', 200, 'reject', '2024-05-11 00:00:00'),
 (4, 'r', 8, 4, 3, '2024-04-26', 'الساعة 4 العصر', 200, 'request', '2024-04-19 00:00:00'),
 (6, 'a', 8, 4, NULL, '2024-04-27', 'الساعة 4 العصر', 200, 'request', '2024-04-19 00:00:00'),
 (7, 'a', 8, 4, NULL, '2024-04-26', 'الساعة 4 العصر', 200, 'request', '2024-04-19 00:00:00'),
-(8, 's', 8, NULL, 5, '2024-04-29', '8:40 PM', 200, 'request', '2024-04-27 00:00:00'),
+(8, 's', 8, NULL, 5, '2024-04-29', '8:40 PM', 200, 'reject', '2024-04-27 00:00:00'),
 (9, 'gfd', 8, NULL, NULL, '2024-04-20', '5:pm', 200, 'request', '2024-04-20 00:00:00'),
 (10, 'any description', 8, 1, NULL, '2024-05-03', '9 in morning', 200, 'request', '2024-04-20 00:00:00'),
-(11, 'any data', 8, NULL, 3, '2024-04-30', 'الساعة 4 العصر', 200, 'request', '2024-04-20 02:17:18');
+(11, 'any data', 8, NULL, 3, '2024-04-30', 'الساعة 4 العصر', 200, 'request', '2024-04-20 02:17:18'),
+(12, 'any', 9, 4, NULL, '2024-05-28', '8:40 PM', 200, 'accept', '2024-05-02 03:31:26'),
+(13, 'سحب دم وقياس الضغط', 9, NULL, 5, '2024-05-24', '2:40 PM', 200, 'accept', '2024-05-06 20:20:55'),
+(14, 'مجارحة وتنظيف جرح في القدم اليسرى', 9, 4, NULL, '2024-05-11', 'الساعة 2 بعد الظهر ', 250, 'accept', '2024-05-11 02:19:19');
 
 -- --------------------------------------------------------
 
@@ -104,7 +107,7 @@ INSERT INTO `doctor` (`id`, `first_name`, `last_name`, `phone`, `email`, `passwo
 (1, 'Ali', 'Naser', '0987654321', 'ali@gmail.com', 'ali', 'Jeddah', 'Interior designer / supervisor', '2019-01-18', '3 years', 'h1.jpg', 'accept'),
 (2, 'q', 'q', 'q', 'q@gmail.com', 'q', 'q', 'q', '2023-02-19', '6 years', 'h3.jpg', 'request'),
 (3, 'dd', 'dd', '09876777', 'dd@gmail.com', 'dd', 'جدة الشارع العام - جوار الحديقة', 'dd', '2024-04-15', '3', 'SimpleInvoc.pdf', 'request'),
-(4, 'doc1', 'doc122', '098765', 'doc1@gmail.com', 'doc1', 'doc122', 'doc1', '2024-04-26', '1', 'invoice1.pdf', 'accept');
+(4, 'doc1', 'doc122', '098765', 'doc1@gmail.com', 'doc1', 'doc122', '11doc1', '2024-04-26', '1', 'invoice1.pdf', 'accept');
 
 -- --------------------------------------------------------
 
@@ -118,6 +121,19 @@ CREATE TABLE `medicine` (
   `name` varchar(255) NOT NULL,
   `detail` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `medicine`
+--
+
+INSERT INTO `medicine` (`id`, `appointment_id`, `name`, `detail`) VALUES
+(1, 12, 'Panadol', '1*2*7 حبتين في اليوم لمدة اسبوعين بعد الصبوح والعشاء'),
+(2, 12, 'Voltarine 25mg', 'amp 1*2 after lunch'),
+(3, 12, 'ceaftricson', '1*2*15'),
+(4, 1, 'Voltarine 25mg', 'amp 1*2 after lunch'),
+(5, 1, 'كورتيزول', 'صباح ومساء حبتين بعد كل وجبه'),
+(6, 13, 'Actifed', 'seab 1*2 '),
+(7, 14, 'sss', ';lkjhg');
 
 -- --------------------------------------------------------
 
@@ -149,7 +165,7 @@ INSERT INTO `nurse` (`id`, `first_name`, `last_name`, `phone`, `email`, `passwor
 (2, 'q', 'q', 'q', 'q@gmail.com', 'q', 'q', 'q', '2023-02-19', '6 years', 'h3.jpg', 'request'),
 (3, '12', '12', '12', '12@gmail.com', '12', 'جدة الشارع العام - جوار الحديقة', '12', '2024-04-01', '12', 'SimpleInvoc.pdf', 'request'),
 (4, 'nono', 'nono', 'nono', 'nono@gmail.com', 'nono', 'nono', 'nono', '2024-04-16', '3', 'SimpleInvoc1.pdf', 'request'),
-(5, 'nur1', 'nur1', '098765', 'nur1@gmail.com', 'nur1', 'nur1', 'nur1', '2024-04-19', '1', 'invoice1.pdf', 'request');
+(5, 'nur1', 'nur1', '098765', 'nur1@gmail.com', 'nur1', 'nur1', 'nur1', '2024-04-19', '2', 'invoice1.pdf', 'accept');
 
 -- --------------------------------------------------------
 
@@ -184,7 +200,8 @@ INSERT INTO `patient` (`id`, `first_name`, `last_name`, `phone`, `email`, `passw
 (3, 'محمد خالد ', 'عبدالرؤف', '7766554434', 'm@gmail.com', 'm', 'جدة الشارع العام - جوار الحديقة', '2024-04-15', 160, 60, 1, '', 1, ''),
 (5, 't', '', '00', 't@gmail.com', '', '', '0000-00-00', 0, 0, 0, NULL, 0, NULL),
 (7, 'hola', '', '876543', 'ssaa3002012@gmail.com', 'hola', '', '0000-00-00', 0, 0, 0, NULL, 0, NULL),
-(8, 'p', 'p', 'p', 'p@gmail.com', 'p', 'p', '2024-04-19', 170, 45, 1, '', 1, 'Fish');
+(8, 'p', 'p', 'p', 'p@gmail.com', 'p', 'p', '2024-04-19', 170, 45, 1, '', 1, 'Fish'),
+(9, 'popop', 'jjjpopo', '0987', 'po@gmail.com', 'po', 'po', '2024-04-21', 189, 34, 1, '', 1, '');
 
 -- --------------------------------------------------------
 
@@ -215,7 +232,7 @@ CREATE TABLE `setting` (
 --
 
 INSERT INTO `setting` (`id`, `appointment_price`) VALUES
-(4, 200);
+(4, 250);
 
 -- --------------------------------------------------------
 
@@ -247,7 +264,8 @@ INSERT INTO `webuser` (`id`, `email`, `usertype`) VALUES
 (11, 'nono@gmail.com', 'n'),
 (12, 'nur1@gmail.com', 'n'),
 (13, 'doc1@gmail.com', 'd'),
-(14, 'p@gmail.com', 'p');
+(14, 'p@gmail.com', 'p'),
+(15, 'po@gmail.com', 'p');
 
 --
 -- Indexes for dumped tables
@@ -328,7 +346,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `doctor`
@@ -340,7 +358,7 @@ ALTER TABLE `doctor`
 -- AUTO_INCREMENT for table `medicine`
 --
 ALTER TABLE `medicine`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `nurse`
@@ -352,7 +370,7 @@ ALTER TABLE `nurse`
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `payment`
@@ -370,7 +388,7 @@ ALTER TABLE `setting`
 -- AUTO_INCREMENT for table `webuser`
 --
 ALTER TABLE `webuser`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
